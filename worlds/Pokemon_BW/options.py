0498572323b55e521785d8a5faf03c40388397c9
Sparky <shockseevee1658@gmail.com>
2025-04-25 14:55:08 +0100

    began options.py

---
+++ worlds/Pokemon_BW/options.py
@@ -0,0 +1,10 @@
+"""
+Option definitions for Pokemon Black and White
+"""
+
+from dataclasses import dataclass
+
+#from Options import (Choice, DeathLink, DefaultOnToggle, OptionSet, NamedRange, Range, Toggle, FreeText,
+#                     PerGameCommonOptions, OptionGroup)
+
+#class goal_flag(Choice):
