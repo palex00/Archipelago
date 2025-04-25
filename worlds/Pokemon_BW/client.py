30215500d65badcb38fd4b1c8c700a33669d86b9
Sparky <shockseevee1658@gmail.com>
2025-04-25 10:32:44 +0100

    Added files and folder structure for PkMn BW AP

---
+++ worlds/Pokemon_BW/client.py
@@ -0,0 +1,27 @@
+#importing libraries
+from typing import TYPE_CHECKING, Dict, Set, Tuple, List
+import struct
+
+from NetUtils import ClientStatus
+import worlds._bizhawk as bizhawk
+from worlds._bizhawk.client import BizHawkClient
+
+import asyncio
+import Utils
+from worlds.LauncherComponents import Component, SuffixIdentifier, Type, components, launch_subprocess
+
+
+if TYPE_CHECKING:
+    from worlds._bizhawk.context import BizHawkClientContext
+else:
+    BizHawkClientContext = object
+
+class PkMnBWClient(BizHawkClient):
+    game = "Pokemon Black and White"
+    system = "NDS"
+    patch_suffix = ".appkmnbw"
+    local_checked_locations: Set[int]
+    local_set_events: Dict[str, bool]
+    local_found_key_items: Dict[str, bool]
+    goal_flag : int
+    
