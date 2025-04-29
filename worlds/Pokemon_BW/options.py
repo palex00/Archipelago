c0cc8443e2b7cc09b6cea9bfcad187e5a82dfd1e
Sparky <shockseevee1658@gmail.com>
2025-04-29 14:01:11 +0100

    assorted changes

--- worlds/Pokemon_BW/options.py
+++ worlds/Pokemon_BW/options.py
@@ -1,10 +1,29 @@
 """
 Option definitions for Pokemon Black and White
 """
-
 from dataclasses import dataclass
 
-#from Options import (Choice, DeathLink, DefaultOnToggle, OptionSet, NamedRange, Range, Toggle, FreeText,
-#                     PerGameCommonOptions, OptionGroup)
+from Options import (Choice, DeathLink, DefaultOnToggle, OptionSet, NamedRange, Range, Toggle, FreeText,
+                     PerGameCommonOptions, OptionGroup)
 
-#class goal_flag(Choice):
+
+class GameVersion(Choice):
+    """
+    Select Black or White version.
+    """
+    display_name = "Game Version"
+    option_black = 0
+    option_white = 1
+    default = "random"
+
+class goal_flag(Choice):
+    """
+    Determines what your goal is to consider the game beaten.
+    - Champion: Become the champion and enter the hall of fame
+    - Gym Challenge: Collect all 8 Gym Badges
+    """
+    display_name = "Goal Flag"
+    default = 0
+    option_champion = 0
+    option_gym_challenge = 1
+
--- worlds/Pokemon_BW/test/__init__.py
+++
@@ -1,5 +0,0 @@
-from test.bases import WorldTestBase
-
-
-class PokemonBWTestBase(WorldTestBase):
-    game = "Pokemon Black and White"
