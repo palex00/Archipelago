7e6232aaae1e675a51923237be883b963e76bbde
Slimey <blastslimeyt@gmail.com>
2025-05-21 00:19:21 +0200

    Added item data and options dataclass

--- worlds/pokemon_bw/data/__init__.py
+++ worlds/pokemon_bw/data/__init__.py
@@ -1,8 +1,9 @@
 from typing import NamedTuple, Callable
 
 from BaseClasses import ItemClassification
+from ..options import PokemonBWOptions
 
 
 class ItemData(NamedTuple):
     id: int
-    classification: Callable[[PokemonBWOptions], ItemClassification]
+    classification: Callable[[PokemonBWOptions, dict[str, any]], ItemClassification]
--- worlds/pokemon_bw/options.py
+++ worlds/pokemon_bw/options.py
@@ -3,12 +3,32 @@ Option definitions for Pokemon Black and White
 """
 from dataclasses import dataclass
 
-from Options import (Choice, PerGameCommonOptions, OptionSet)
+from Options import (Choice, PerGameCommonOptions, OptionSet, Range, Toggle)
+
+
+class Goal(Choice):
+    """
+    Determines what your goal is to consider the game beaten.
+    - **Ghetsis** - Clear the main story by defeating Ghetsis
+    - **Champion** - Become the champion by defeating Alder
+    - **Cynthia** - Defeat Cynthia in Undella Town
+    - **Full pokédex** - Complete the pokédex
+    - **TM/HM hunt** - Get all TMs and HMs
+    - **Seven Sages hunt** - Find the Seven Sages
+    """
+    display_name = "Goal"
+    default = 0
+    option_ghetsis = 0
+    option_champion = 1
+    option_cynthia = 2
+    option_full_pokedex = 3
+    option_tmhm_hunt = 4
+    option_seven_sages_hunt = 5
 
 
 class GameVersion(Choice):
     """
-    Select Black or White version.
+    Select your version.
     """
     display_name = "Game Version"
     option_black = 0
@@ -16,50 +36,57 @@ class GameVersion(Choice):
     default = "random"
 
 
-class Goal(Choice):
-    """
-    Determines what your goal is to consider the game beaten.
-    - **Champion** - Defeat Alder
-    - **Cynthia** - Defeat cynthia
-    - **Fulldex** - Finish the Pokedex
-    - **TM/HM hunt** - Find an amount of TMs and HMs
-    - **Seven Sages** - Find the Seven Sages
-    """
-    display_name = "Goal"
-    default = 0
-    option_champion = 0
-    option_cynthia = 1
-    option_fulldex = 2
-    option_tmhm = 3
-    option_sages = 4
-
-
 class RandomizeWildPokemon(OptionSet):
     """
     Randomizes wild Pokémon encounters.
     - **Randomize** - Toggles wild Pokémon being randomized. Required for any other modifier.
     - **Similar base stats** - Tries to keep the randomized Pokémon at a similar base stat total as the vanilla encounter.
-    - **Type themed** -
+    - **Type themed** - Makes every Pokémon in an area have a certain same type.
+    - **Keep variety** - Keeps the amount of different encounters and their encounter rate in every area.
     """
+    display_name = "Randomize wild Pokémon"
+    valid_keys = [
+        "Randomize",
+        "Similar base stats",
+        "Type themed",
+        "Keep variety",
+    ]
 
 
-class Dexsanity(Choice):
+class RandomizeStarterPokemon(OptionSet):
     """
-    Enables Dexsanity
+    Randomizes the starter Pokémon you receive at the start of the game.
+    - **Randomize** - Toggles wild Pokémon being randomized. Required for any other modifier.
+    - **Any base** - Only use unevolved Pokémon.
+    - **Base with 2 evolutions** - Only use unevolved Pokémon that can evolve twice. Overrides **Any base**.
+    - **Type variety** - Every starter will have a single type that is different from the other two.
+    """
+    display_name = "Randomize wild Pokémon"
+    valid_keys = [
+        "Randomize",
+        "Any base",
+        "Base with 2 evolutions",
+        "Type variety",
+    ]
+
+
+class Dexsanity(Range):
+    """
+    Adds a number of locations that can be checked by catching a certain Pokémon species
     """
     display_name = "Dexsanity"
     default = 0
-    option_off = 0
-    option_on = 1
+    range_start = 0
+    range_end = 649
 
-class FunnyDialogue(Choice):
+
+class FunnyDialogue(Toggle):
     """
-    Adds humorous dialogue submitted by the folks in the Pokemon Black and White thread of the Archipelago Discord server
+    Adds humorous dialogue submitted by the folks in the Pokèmon Black and White thread of the Archipelago Discord server
     """
     display_name = "FunnyDialogue"
     default = 0
-    option_off = 0
-    option_on = 1
+
 
 class ReusableTMs(Choice):
     """
@@ -72,31 +99,13 @@ class ReusableTMs(Choice):
     option_of_course = 2
     option_im_not_a_masochist = 3
 
-class DeathLink(Choice):
-    """
-    Toggles DeathLink. (not currently functional)
-    """
-    display_name = "DeathLink"
-    default = 0
-    option_off = 0
-    option_on = 1
-
 
 @dataclass
 class PokemonBWOptions(PerGameCommonOptions):
     goal: Goal
-
-    badges: RandomizeBadges
-    hms: RandomizeHms
-    key_items: RandomizeKeyItems
-    bikes: RandomizeBikes
-    event_tickets: RandomizeEventTickets
-    rods: RandomizeRods
-    overworld_items: RandomizeOverworldItems
-    hidden_items: RandomizeHiddenItems
-    npc_gifts: RandomizeNpcGifts
-    berry_trees: RandomizeBerryTrees
+    version: GameVersion
+    randomize_wild_pokemon: RandomizeWildPokemon
+    randomize_starter_pokemon: RandomizeStarterPokemon
     dexsanity: Dexsanity
-    trainersanity: Trainersanity
-    item_pool_type: ItemPoolType
-
+    funny_dialogue: FunnyDialogue
+    reusable_tms: ReusableTMs
