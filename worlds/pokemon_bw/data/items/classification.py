57bca66d58d61f38d906bfc59fe5db1166eac2eb
Slimey <blastslimeyt@gmail.com>
2025-05-26 01:44:57 +0200

    Added basic item classification callables

--- worlds/pokemon_bw/data/items/classification.py
+++ worlds/pokemon_bw/data/items/classification.py
@@ -1,2 +1,20 @@
+from typing import Callable
+
+from BaseClasses import ItemClassification
+from ...options import PokemonBWOptions
 
 
+def always_progression(options: PokemonBWOptions, data: dict[str, any]) -> ItemClassification:
+    return ItemClassification.progression
+
+
+def always_useful(options: PokemonBWOptions, data: dict[str, any]) -> ItemClassification:
+    return ItemClassification.useful
+
+
+def always_filler(options: PokemonBWOptions, data: dict[str, any]) -> ItemClassification:
+    return ItemClassification.filler
+
+
+def always_trap(options: PokemonBWOptions, data: dict[str, any]) -> ItemClassification:
+    return ItemClassification.trap
