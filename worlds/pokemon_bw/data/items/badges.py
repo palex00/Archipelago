d00e944feea4e72a94498309c05df51a55ceeed2
Slimey <blastslimeyt@gmail.com>
2025-05-27 20:15:56 +0200

    Fixed badge items

--- worlds/pokemon_bw/data/items/badges.py
+++ worlds/pokemon_bw/data/items/badges.py
@@ -3,11 +3,11 @@ from .. import BadgeItemData
 
 item_table: dict[str, BadgeItemData] = {
     "Trio Badge": BadgeItemData(0, always_progression),
-    "Basic Badge": BadgeItemData(0, always_progression),
-    "Insect Badge": BadgeItemData(0, always_progression),
-    "Bolt Badge": BadgeItemData(0, always_progression),
-    "Quake Badge": BadgeItemData(0, always_progression),
-    "Jet Badge": BadgeItemData(0, always_progression),
-    "Freeze Badge": BadgeItemData(0, always_progression),
-    "Legend Badge": BadgeItemData(0, always_progression),
+    "Basic Badge": BadgeItemData(1, always_progression),
+    "Insect Badge": BadgeItemData(2, always_progression),
+    "Bolt Badge": BadgeItemData(3, always_progression),
+    "Quake Badge": BadgeItemData(4, always_progression),
+    "Jet Badge": BadgeItemData(5, always_progression),
+    "Freeze Badge": BadgeItemData(6, always_progression),
+    "Legend Badge": BadgeItemData(7, always_progression),
 }
