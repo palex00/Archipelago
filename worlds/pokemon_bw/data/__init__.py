c4b7c49cdb6b8cae3f0c63325093b7ad28c4c747
BlastSlimey <89539656+BlastSlimey@users.noreply.github.com>
2025-06-06 15:44:18 +0200

    Added more data definitions

--- worlds/pokemon_bw/data/__init__.py
+++ worlds/pokemon_bw/data/__init__.py
@@ -1,12 +1,14 @@
-from typing import NamedTuple, Callable
+from typing import NamedTuple, Callable, Literal
 
-from BaseClasses import ItemClassification, LocationProgressType
+from BaseClasses import ItemClassification, LocationProgressType, CollectionState
 from ..options import PokemonBWOptions
 
 
 class ItemData(NamedTuple):
     id: int
+    # Takes the options and extra data and returns the classification (progression, useful, filler, or trap) for the calling world
     classification: Callable[[PokemonBWOptions, dict[str, any]], ItemClassification]
+    bag: Literal["Main", "Key", "Medicine", "Berries", "TM/HM"]
 
 
 class BadgeItemData(NamedTuple):
@@ -17,7 +19,52 @@ class BadgeItemData(NamedTuple):
 class LocationData(NamedTuple):
     address_black: int
     address_white: int
-    checking_type_black: Callable[[int], bool]
-    checking_type_white: Callable[[int], bool]
+    # Takes the address, does something with it (e.g. check for value or certain bit) and returns whether the location was checked
+    checking_type: Callable[[int], bool]
+    # Takes the options and extra data and returns the progress type (NORMAL, PRIORITY, or EXCLUDED) for the calling world
     progress_type: Callable[[PokemonBWOptions, dict[str, any]], LocationProgressType]
     region: str
+    # An extra rule that is not covered by region access (e.g. items behind a strength boulder)
+    rule: Callable[[CollectionState], bool] | None
+
+
+class EncounterEventData(NamedTuple):
+    species_black: str
+    species_white: str
+    encounter_region: str
+
+
+class RegionConnectionData(NamedTuple):
+    exiting_region: str
+    entering_region: str
+    rule: Callable[[CollectionState], bool] | None
+
+
+class SpeciesData(NamedTuple):
+    id: int
+    type_1: str
+    type_2: str
+    catch_rate: int
+    base_hp: int
+    base_attack: int
+    base_defense: int
+    base_sp_attack: int
+    base_sp_defense: int
+    base_speed: int
+    gender_ratio: int
+    level_up_moves: list[tuple[int, str]]
+    tm_hm_moves: list[int]
+
+
+class MoveData(NamedTuple):
+    type: str
+    category: Literal["Physical", "Special"]
+    power: int
+    # (Number of positive effects) - (Number of negative effects)
+    effects_difference: int
+    ap: int
+
+
+class TMHMData(NamedTuple):
+    move: str
+    is_HM: bool
