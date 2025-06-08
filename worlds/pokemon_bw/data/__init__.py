5a4f9d95836ba23f792ed237b3c677b74d655837
Slimey <blastslimeyt@gmail.com>
2025-06-08 02:44:28 +0200

    Even more data definitions

--- worlds/pokemon_bw/data/__init__.py
+++ worlds/pokemon_bw/data/__init__.py
@@ -6,7 +6,8 @@ from ..options import PokemonBWOptions
 
 class ItemData(NamedTuple):
     id: int
-    # Takes the options and extra data and returns the classification (progression, useful, filler, or trap) for the calling world
+    # Takes the options and extra data and returns the classification (progression, useful, filler, or trap)
+    # for the calling world
     classification: Callable[[PokemonBWOptions, dict[str, any]], ItemClassification]
     bag: Literal["Main", "Key", "Medicine", "Berries", "TM/HM"]
 
@@ -19,19 +20,25 @@ class BadgeItemData(NamedTuple):
 class LocationData(NamedTuple):
     address_black: int
     address_white: int
-    # Takes the address, does something with it (e.g. check for value or certain bit) and returns whether the location was checked
+    # Takes the read value, does something with it (e.g. check for value or certain bit)
+    # and returns whether the location was checked
     checking_type: Callable[[int], bool]
-    # Takes the options and extra data and returns the progress type (NORMAL, PRIORITY, or EXCLUDED) for the calling world
+    # Takes the options and extra data and returns the progress type (NORMAL, PRIORITY, or EXCLUDED)
+    # for the calling world
     progress_type: Callable[[PokemonBWOptions, dict[str, any]], LocationProgressType]
     region: str
     # An extra rule that is not covered by region access (e.g. items behind a strength boulder)
     rule: Callable[[CollectionState], bool] | None
 
 
-class EncounterEventData(NamedTuple):
-    species_black: str
-    species_white: str
+class EncounterData(NamedTuple):
+    # (dex number, form)
+    species_black: tuple[int, int]
+    species_white: tuple[int, int]
     encounter_region: str
+    # The following will become important when wild encounter randomization happens
+    # file_number: int
+    # offset: int
 
 
 class RegionConnectionData(NamedTuple):
@@ -41,30 +48,52 @@ class RegionConnectionData(NamedTuple):
 
 
 class SpeciesData(NamedTuple):
-    id: int
+    dex_number: int
+    form: int
     type_1: str
     type_2: str
-    catch_rate: int
     base_hp: int
     base_attack: int
     base_defense: int
     base_sp_attack: int
     base_sp_defense: int
     base_speed: int
+    catch_rate: int
     gender_ratio: int
+    # starts with 0 for base evolutions
+    evolution_stage: int
+    # (primary, secondary, hidden)
+    abilities: tuple[str, str, str]
+    # tuple(method, parameter, evolve into)
+    evolutions: list[tuple[str, int, str]]
+
+
+class MovesetData(NamedTuple):
+    # tuple(level, move name)
     level_up_moves: list[tuple[int, str]]
-    tm_hm_moves: list[int]
+    # TM number (101+ are HMs)
+    tm_hm_moves: set[int]
 
 
 class MoveData(NamedTuple):
     type: str
-    category: Literal["Physical", "Special"]
+    category: Literal["Physical", "Special", "Status"]
     power: int
     # (Number of positive effects) - (Number of negative effects)
     effects_difference: int
-    ap: int
+    pp: int
 
 
 class TMHMData(NamedTuple):
     move: str
     is_HM: bool
+
+
+class EvolutionMethodData(NamedTuple):
+    id: int
+    # The access rule for dexsanity logic
+    rule: Callable[[CollectionState], bool]
+
+
+class TypeData(NamedTuple):
+    id: int
