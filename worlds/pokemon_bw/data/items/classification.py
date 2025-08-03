939662db421dd5090d2b8a0774ae2b25966a2415
Slimey <blastslimeyt@gmail.com>
2025-08-03 23:12:35 +0200

    lol

--- worlds/pokemon_bw/data/items/classification.py
+++ worlds/pokemon_bw/data/items/classification.py
@@ -2,15 +2,15 @@ from BaseClasses import ItemClassification
 from .. import ClassificationMethod
 
 
-always_progression: ClassificationMethod = lambda options, world: ItemClassification.progression
+always_progression: ClassificationMethod = lambda world: ItemClassification.progression
 
-always_useful: ClassificationMethod = lambda options, world: ItemClassification.useful
+always_useful: ClassificationMethod = lambda world: ItemClassification.useful
 
-always_filler: ClassificationMethod = lambda options, world: ItemClassification.filler
+always_filler: ClassificationMethod = lambda world: ItemClassification.filler
 
-always_trap: ClassificationMethod = lambda options, world: ItemClassification.trap
+always_trap: ClassificationMethod = lambda world: ItemClassification.trap
 
-tm_hm_hunt: ClassificationMethod = lambda options, world: (
+tm_hm_hunt: ClassificationMethod = lambda world: (
     ItemClassification.progression
-    if options.goal == "tmhm_hunt" else ItemClassification.useful
+    if world.options.goal == "tmhm_hunt" else ItemClassification.useful
 )
