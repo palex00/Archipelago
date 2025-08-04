476672c2a5ec93e36cca8511f0b5ee0e3a7a6c1b
Slimey <blastslimeyt@gmail.com>
2025-08-05 01:34:30 +0200

    Event and available pokémon generating functions

---
+++ worlds/pokemon_bw/generate/events/evolutions.py
@@ -0,0 +1,70 @@
+from typing import TYPE_CHECKING, Callable
+
+from ...locations import PokemonBWLocation
+from BaseClasses import ItemClassification, CollectionState
+from ...items import PokemonBWItem
+
+if TYPE_CHECKING:
+    from ... import PokemonBWWorld
+    from BaseClasses import Region
+    from ...data import SpeciesData, EvolutionMethodData, ExtendedRule, RulesDict
+
+
+def create(world: PokemonBWWorld, regions: dict[str, Region], catchable_dex_form: set[tuple[str, int]]) -> None:
+    from ...data.pokemon.species import by_id as species_by_id, by_name as species_by_name
+    from ...data.pokemon.pokedex import by_name as pokedex_by_name
+    from ...data.pokemon.movesets import table as movesets_table
+    from ...data.pokemon.evolution_methods import methods, extended_rules_list
+
+    region: Region = regions["Evolutions"]
+    rules: RulesDict = {ext_rule: (lambda state: ext_rule(state, world)) for ext_rule in extended_rules_list}
+    new_catchable: set[tuple[str, int]] = catchable_dex_form.copy()
+    next_catchable: set[tuple[str, int]] = set()
+
+    def get_rule(current_evolution: tuple[str, int, str]) -> Callable[[CollectionState], bool]:
+        method: EvolutionMethodData = methods[current_evolution[0]]
+        ext_rule: ExtendedRule = method.rule(current_evolution[1])
+        if ext_rule in rules:
+            return rules[ext_rule]
+        else:
+            return lambda state: ext_rule(state, world)
+
+    while len(new_catchable) > 0:
+        for dex_form in new_catchable:
+            dex_num: int = pokedex_by_name[dex_form[0]]
+            species_name: str = species_by_id[(dex_num, dex_form[1])]
+            data: SpeciesData = species_by_name[species_name]
+
+            for x in range(len(data.evolutions)):
+                evolution: tuple[str, int, str] = data.evolutions[x]
+                name = f"Evolving {species_name} {x+1}"
+                location: PokemonBWLocation = PokemonBWLocation(world.player, name, None, region)
+                item: PokemonBWItem = PokemonBWItem(evolution[2], ItemClassification.progression, None, world.player)
+                location.place_locked_item(item)
+                location.access_rule = get_rule(evolution)
+                region.locations.append(location)
+
+                evo_data: SpeciesData = species_by_name[evolution[2]]
+                evo_dex_form: tuple[str, int] = (evo_data.dex_name, evo_data.form)
+                catchable_dex_form.add(evo_dex_form)
+                next_catchable.add(evo_dex_form)
+                moveset: set[str] = movesets_table[evolution[2]].tm_hm_moves
+                if "HM04" in moveset:
+                    world.strength_species.add(evolution[2])
+                if "HM01" in moveset:
+                    world.cut_species.add(evolution[2])
+                if "HM03" in moveset:
+                    world.surf_species.add(evolution[2])
+                if "HM06" in moveset:
+                    world.dive_species.add(evolution[2])
+                if "HM05" in moveset:
+                    world.waterfall_species.add(evolution[2])
+                if "Fighting" in (evo_data.type_1, evo_data.type_2):
+                    world.fighting_type_species.add(evolution[2])
+
+        new_catchable = next_catchable
+        next_catchable = set()
+
+
+
+
---
+++ worlds/pokemon_bw/generate/events/wild.py
@@ -0,0 +1,45 @@
+from typing import TYPE_CHECKING
+
+from ...locations import PokemonBWLocation
+from BaseClasses import ItemClassification
+from ...items import PokemonBWItem
+
+if TYPE_CHECKING:
+    from ... import PokemonBWWorld
+    from BaseClasses import Region
+    from ...data import SpeciesData
+
+
+def create(world: PokemonBWWorld, regions: dict[str, Region]) -> set[tuple[str, int]]:
+    from ...data.locations.encounters.slots import table
+    from ...data.pokemon.species import by_id as species_by_id, by_name as species_by_name
+    from ...data.pokemon.movesets import table as movesets_table
+
+    catchable_dex_form: set[tuple[str, int]] = set()
+
+    for name, data in table.items():
+        r: Region = regions[data.encounter_region]
+        l: PokemonBWLocation = PokemonBWLocation(world.player, name, None, r)
+        species_id: tuple[int, int] = data.species_black if world.options.version == "black" else data.species_white
+        species_name: str = species_by_id[species_id]
+        item: PokemonBWItem = PokemonBWItem(species_name, ItemClassification.progression, None, world.player)
+        l.place_locked_item(item)
+        r.locations.append(l)
+
+        species_data: SpeciesData = species_by_name[species_name]
+        catchable_dex_form.add((species_data.dex_name, species_id[1]))
+        moveset: set[str] = movesets_table[species_name].tm_hm_moves
+        if "HM04" in moveset:
+            world.strength_species.add(species_name)
+        if "HM01" in moveset:
+            world.cut_species.add(species_name)
+        if "HM03" in moveset:
+            world.surf_species.add(species_name)
+        if "HM06" in moveset:
+            world.dive_species.add(species_name)
+        if "HM05" in moveset:
+            world.waterfall_species.add(species_name)
+        if "Fighting" in (species_data.type_1, species_data.type_2):
+            world.fighting_type_species.add(species_name)
+
+    return catchable_dex_form
