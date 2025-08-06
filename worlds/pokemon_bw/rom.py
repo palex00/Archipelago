327b3b61107a45c165639e29f6340dbe67ac0e21
Slimey <blastslimeyt@gmail.com>
2025-08-06 02:35:57 +0200

    Rom generating and patching part 1

--- worlds/pokemon_bw/rom.py
+++ worlds/pokemon_bw/rom.py
@@ -1,27 +1,96 @@
-import math
-import hashlib
-import itertools
-import struct
 import os
-# import bsdiff4
-import pkgutil
-import unicodedata
+import zipfile
 
-from typing import TYPE_CHECKING
-import settings
+import ndspy.rom
+
 import Utils
-from worlds.Files import APDeltaPatch
-
-from . import Items
-from .options import Dexsanity
+from settings import get_settings
+from worlds.Files import APPatch, AutoPatchExtensionRegister
+from typing import TYPE_CHECKING, Any, Dict, ClassVar, Callable
 
 if TYPE_CHECKING:
-    from . import PkMnBW
+    from . import PokemonBWWorld
 
 
-# ROM ADDRESSES
-class Addr:
-    Pokemon = 0x0
-    Items = 0x0
-    Seasons = 0x0
+patch_procedures: dict[str, Callable] = {
+    # TODO list procedures from patch module
+}
 
+
+class PokemonBWPatch(APPatch):
+    game = "Pokemon Black and White"
+    bw_patch_version = (0, 1, 0)
+    patch_file_ending_black = ".apblack"
+    patch_file_ending_white = ".apwhite"
+    result_file_ending = ".nds"
+
+    def __init__(self, world: PokemonBWWorld | None, *args: Any, **kwargs: Any):
+        self.game_version = None
+        self.world = world
+        self.files: dict[str, bytes] = {}
+        super().__init__(*args, **kwargs)
+
+    def write_contents(self, opened_zipfile: zipfile.ZipFile) -> None:
+        super().write_contents(opened_zipfile)
+
+        procedures: list[str] = ["base_patch"]
+        if self.world.options.season_control:
+            procedures.append("season_patch_"+self.world.options.version.current_key)
+
+        opened_zipfile.writestr("procedures.txt", "\n".join(procedures))
+
+    def get_manifest(self) -> Dict[str, Any]:
+        manifest = super().get_manifest()
+        manifest["bw_patch_version"] = self.bw_patch_version
+        manifest["game_version"] = self.world.options.version.current_key
+        return manifest
+
+    def patch(self, target: str) -> None:
+        self.read()
+        base_data = get_base_rom_bytes(self.game_version)
+
+        rom = ndspy.rom.NintendoDSRom(base_data)
+        procedures: list[str] = str(self.get_file("procedures.txt"), "utf-8").splitlines()
+        for prod in procedures:
+            patch_procedures[prod]()  # TODO params
+
+        with open(target, 'wb') as f:
+            f.write(rom.save(updateDeviceCapacity=True))
+
+    def read_contents(self, opened_zipfile: zipfile.ZipFile) -> Dict[str, Any]:
+        for file in opened_zipfile.namelist():
+            if file not in ["archipelago.json"]:
+                self.files[file] = opened_zipfile.read(file)
+
+        manifest = super().read_contents(opened_zipfile)
+        if manifest["bw_patch_version"] > self.bw_patch_version:
+            raise Exception(f"File (BW patch version: {".".join(manifest["bw_patch_version"])} too new "
+                            f"for this handler (BW patch version: {self.bw_patch_version})")
+        self.game_version = manifest["game_version"]
+        return manifest
+
+    def get_file(self, file: str) -> bytes:
+        """ Retrieves a file from the patch container."""
+        if file not in self.files:
+            self.read()
+        return self.files[file]
+
+
+def get_base_rom_bytes(version: str, file_name: str = "") -> bytes:
+    if not file_name:
+        file_name = get_base_rom_path(version, file_name)
+    with open(file_name, "rb") as file:
+        base_rom_bytes = bytes(file.read())
+    if version == "black" and base_rom_bytes[:18] != b'POKEMON\x20B\0\0\0IRBO01':
+        raise Exception(f"Supplied Base Rom appears to not be an english copy of Pokémon Black Version.")
+    if version == "white" and base_rom_bytes[:18] != b'POKEMON\x20W\0\0\0IRAO01':
+        raise Exception(f"Supplied Base Rom appears to not be an english copy of Pokémon White Version.")
+    return base_rom_bytes
+
+
+def get_base_rom_path(version: str, file_name: str = "") -> str:
+    if not file_name:
+        file_name = get_settings()["pokemon_bw_settings"][f"{version}_rom"]
+    if not os.path.exists(file_name):
+        file_name = Utils.user_path(file_name)
+    return file_name
