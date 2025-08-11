d05720dbd524cb1bad78d7529cda633a5b02c7e2
Slimey <blastslimeyt@gmail.com>
2025-08-11 19:37:12 +0200

    Fixed season patch leading to whitescreens

--- worlds/pokemon_bw/patch/procedures/season_patch.py
+++ worlds/pokemon_bw/patch/procedures/season_patch.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
 
 def patch(rom: NintendoDSRom, world_package: str, bw_patch_instance: "PokemonBlackPatch") -> None:
     otpp_patch: bytes = pkgutil.get_data(world_package, "patch/seasons_otpp/ov20_decomp")
-    overlay_table = rom.loadArm9Overlays([20])
+    overlay_table = rom.loadArm9Overlays()
     ov20 = overlay_table[20]
     ov20.data = otpp.patch(ov20.data, otpp_patch)
     rom.files[ov20.fileID] = ov20.save(compress=True)
